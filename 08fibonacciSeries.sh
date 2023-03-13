8a983a6dfc2b2c0d7e793a306ccd65599e69e28a
Sai Ravula <hellosai.devops@gmail.com>
2023-03-13 18:25:51 +0530

    Added line numbers to fibonacci series

--- 08fibonacciSeries.sh
+++ 08fibonacciSeries.sh
@@ -4,12 +4,19 @@
 read -p "Enter the range upto which Fibonacci is required: " range;
 first=0;
 second=1;
+line_num=1;
+echo -n "$line_num ";
+line_num=$((line_num+=1));
 echo $first;
+echo -n "$line_num ";
 echo $second;
+line_num=$((line_num+=1));
 third=$(( first + second ));
 while [ $third -lt $range ];
 do
+echo -n "$line_num ";
 echo $third;
+line_num=$((line_num+=1));
 first=$second;
 second=$third;
 third=$(( first + second ));
